512d83b6d3b395ec4644710dfd45fbabb3ca0f95
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-12-21 14:05:04 +0100

    Adds generate-page

--- index.js
+++ index.js
@@ -2,8 +2,14 @@
 
 const { get } = require('got')
 const { json, send } = require('micro')
+const generatePage = require('./lib/generate-page')
 
 module.exports = async (request, response) => {
-  const skoleskyssData = await get('https://tfk-livestats.firebaseio.com/skoleskyss.json', {json: true})
-  send(response, 200, skoleskyssData.body)
+  const skoleskyss = await get('https://tfk-livestats.firebaseio.com/skoleskyss.json', {json: true})
+  const minelev = await get('https://tfk-livestats.firebaseio.com/minelev.json', {json: true})
+  const tilskudd = await get('https://tfk-stats.firebaseio.com/tilskudd.json', {json: true})
+  const skoleskyssData = skoleskyss.body
+  const tilskuddData = tilskudd.body
+  const minelevData = minelev.body
+  send(response, 200, generatePage(minelevData, skoleskyssData, tilskuddData))
 }
