8adbd718d579e6ad0ffe07cd2c902b162b9b36a9
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-12-26 19:59:23 +0100

    Refactoring to Promise.all

--- index.js
+++ index.js
@@ -3,11 +3,14 @@
 const { get } = require('got')
 const { send } = require('micro')
 const generatePage = require('./lib/generate-page')
+const urls = [
+  'https://tfk-livestats.firebaseio.com/skoleskyss.json',
+  'https://tfk-livestats.firebaseio.com/minelev.json',
+  'https://tfk-stats.firebaseio.com/tilskudd.json'
+]
 
 module.exports = async (request, response) => {
-  const skoleskyss = await get('https://tfk-livestats.firebaseio.com/skoleskyss.json', {json: true})
-  const minelev = await get('https://tfk-livestats.firebaseio.com/minelev.json', {json: true})
-  const tilskudd = await get('https://tfk-stats.firebaseio.com/tilskudd.json', {json: true})
+  const [skoleskyss, minelev, tilskudd] = await Promise.all(urls.map((url) => get(url, {json: true})))
   const skoleskyssData = skoleskyss.body
   const tilskuddData = tilskudd.body
   const minelevData = minelev.body
