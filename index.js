1f3f9f4317f6041279a3be0b48bb399a899697ae
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-12-27 22:24:02 +0100

    Makes index generated by config

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const generatePage = require('./lib/generate-page')
 const systems = require('./config/systems')
 
 module.exports = async (request, response) => {
-  const [skoleskyss, minelev, tilskudd] = await Promise.all(systems.map((item) => get(item.url, {json: true})))
-  send(response, 200, generatePage(minelev.body, skoleskyss.body, tilskudd.body))
+  const data = await Promise.all(systems.map((item) => get(item.url, {json: true})))
+  send(response, 200, generatePage(data.map((site) => site.body)))
 }
