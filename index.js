28b6c170f41392e65d9a7d024e369a8a859241f5
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-12-27 08:50:55 +0100

    Small refactoring

--- index.js
+++ index.js
@@ -7,8 +7,5 @@ const systems = require('./config/systems')
 
 module.exports = async (request, response) => {
   const [skoleskyss, minelev, tilskudd] = await Promise.all(systems.map((item) => get(item.url, {json: true})))
-  const skoleskyssData = skoleskyss.body
-  const tilskuddData = tilskudd.body
-  const minelevData = minelev.body
-  send(response, 200, generatePage(minelevData, skoleskyssData, tilskuddData))
+  send(response, 200, generatePage(minelev.body, skoleskyss.body, tilskudd.body))
 }
