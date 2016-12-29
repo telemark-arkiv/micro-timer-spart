61df672a7dcb86052e5f1f53e787482f1b2d894a
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-12-29 10:48:08 +0100

    More cleanup

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ const { get } = require('got')
 const { send } = require('micro')
 const generatePage = require('./lib/generate-page')
 const systems = require('./config/systems')
+const totalSum = (prev, actual) => prev + actual
 
 module.exports = async (request, response) => {
   const data = await Promise.all(Object.keys(systems).map((itemKey) => get(systems[itemKey].url, {json: true})))
-  send(response, 200, generatePage(data.map((site) => site.body)))
+  const minutterSpart = data
+                          .map((response) => response.body)
+                          .map((site) => Object.assign(site, systems[site.systemid]))
+                          .map((system) => system[system.field] * system.factor)
+                          .reduce(totalSum, 0)
+
+  const timerSpart = minutterSpart / 60
+
+  send(response, 200, generatePage(timerSpart))
 }
