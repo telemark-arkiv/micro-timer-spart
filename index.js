4fe5c8ad8fda40d17cbc900cb7a0f7a76b6dd37e
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-01-03 14:58:11 +0100

    Adds support for json (feat)

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 'use strict'
 
+const { parse } = require('url')
 const { get } = require('got')
 const { send } = require('micro')
 const generatePage = require('./lib/generate-page')
 const totalSum = (prev, actual) => prev + actual
 
 module.exports = async (request, response) => {
+  const query = parse(request.url, true).query
+  const format = query ? query.format : false
   const systems = (await get('https://systems.config.tfk.allthethings.win', {json: true})).body
   const data = await Promise.all(Object.keys(systems).map((itemKey) => get(systems[itemKey].url, {json: true})))
   const minutterSpart = data
@@ -14,7 +17,7 @@ module.exports = async (request, response) => {
                           .map((system) => system[system.field] * system.factor)
                           .reduce(totalSum, 0)
 
-  const timerSpart = minutterSpart / 60
+  const results = {timerSpart: minutterSpart / 60}
 
-  send(response, 200, generatePage(timerSpart))
+  send(response, 200, format === 'json' ? results : generatePage(results))
 }
