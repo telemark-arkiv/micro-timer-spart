da69a7f8ff2d5949abda2ff7e585df381a18b935
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-01-24 12:07:31 +0100

    Adds cors for /json (minor)

--- index.js
+++ index.js
@@ -10,20 +10,24 @@ const totalSum = (prev, actual) => prev + actual
 
 module.exports = async (request, response) => {
   const { pathname } = await parse(request.url, true)
-  const systems = (await get('https://systems.config.tfk.allthethings.win', {json: true})).body
-  const data = await Promise.all(Object.keys(systems).map((itemKey) => get(systems[itemKey].url, {json: true})))
-  const minutterSpart = data
-                          .map((response) => response.body)
-                          .map((site) => Object.assign(site, systems[site.systemid]))
-                          .map((system) => system[system.field] * system.factor)
-                          .reduce(totalSum, 0)
+  if (pathname === '/json' || pathname === '/html') {
+    const systems = (await get('https://systems.config.tfk.allthethings.win', {json: true})).body
+    const data = await Promise.all(Object.keys(systems).map((itemKey) => get(systems[itemKey].url, {json: true})))
+    const minutterSpart = data
+      .map((response) => response.body)
+      .map((site) => Object.assign(site, systems[site.systemid]))
+      .map((system) => system[system.field] * system.factor)
+      .reduce(totalSum, 0)
 
-  const results = [{name: 'Timer spart', status: minutterSpart / 60}]
+    const results = [{name: 'Timer spart', status: minutterSpart / 60}]
 
-  if (pathname === '/json') {
-    send(response, 200, results)
-  } else if (pathname === '/html') {
-    send(response, 200, renderPage(results))
+    if (pathname === '/json') {
+      response.setHeader('Access-Control-Allow-Origin', '*')
+      response.setHeader('Access-Control-Allow-Methods', 'GET')
+      send(response, 200, results)
+    } else if (pathname === '/html') {
+      send(response, 200, renderPage(results))
+    }
   } else {
     const readme = readFileSync('./README.md', 'utf-8')
     const html = marked(readme)
